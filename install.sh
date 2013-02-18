cb93ccfe7257c0cc4cd86e7b6a49bca087768cec
fisto <yellow_piero@yahoo.co.jp>
2013-02-18 19:53:40 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ echo "source ~/.dotfiles/.vimrc" >> ~/.vimrc
 echo "source ~/.dotfiles/.zshrc" >> ~/.zshrc
 echo "source ~/.dotfiles/.bashrc" >> ~/.bashrc
 
-ln -Fis .git* $HOME
+ln -Fis ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -Fis ~/.dotfiles/.gitgitignore ~/.gitignore
