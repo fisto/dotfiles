251561b189ff76e37fa2c362403458be398f18e2
fisto <cat.fisto@gmail.com>
2014-10-06 00:00:16 +0900

    add neobundle clone sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ echo "source ~/.dotfiles/.bashrc" >> ~/.bashrc
 ln -Fis ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -Fis ~/.dotfiles/.gitgitignore ~/.gitignore
 
+git clone git://github.com/Shougo/neobundle.vim ~/.dotfiles/.vim.d/neobundle.vim
 echo "Finished."
