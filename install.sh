459c269c21ddc6e2816878eaba32e21e6832e15c
fisto <cat.fisto@gmail.com>
2019-03-04 19:13:14 +0900

    Rename files

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo "source ~/.dotfiles/.vimrc" >> ~/.vimrc
-echo "source ~/.dotfiles/.zshrc" >> ~/.zshrc
-#echo "source ~/.dotfiles/.bashrc" >> ~/.bashrc
+echo "source ~/.dotfiles/vimrc" >> ~/.vimrc
+# echo "source ~/.dotfiles/.zshrc" >> ~/.zshrc
+echo "source ~/.dotfiles/bashrc" >> ~/.bashrc
 
-ln -Fis ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -Fis ~/.dotfiles/.gitgitignore ~/.gitignore
+ln -Fis ~/.dotfiles/gitconfig ~/.gitconfig
+ln -Fis ~/.dotfiles/gitgitignore ~/.gitignore
 
 # tmux.conf
-ln -Fis ~/.dotfiles/.tmux.conf ~/.tmux.conf
+# ln -Fis ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 # NeoBundle
 git clone git://github.com/Shougo/neobundle.vim ~/.dotfiles/.vim.d/neobundle.vim
